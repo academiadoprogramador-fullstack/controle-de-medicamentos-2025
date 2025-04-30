ac1526404419060b97ef8c47a9a9636db8a79e47
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-04-30 14:51:57 -0300

    Implementado boilerplate das pastas compartilhadas

--- ControleDeMedicamentos.ConsoleApp/Program.cs
+++ ControleDeMedicamentos.ConsoleApp/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace ControleDeMedicamentos.ConsoleApp
+﻿using ControleDeMedicamentos.ConsoleApp.Compartilhado;
+using ControleDeMedicamentos.ConsoleApp.Util;
+
+namespace ControleDeMedicamentos.ConsoleApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // boilerplate
+            TelaPrincipal telaPrincipal = new TelaPrincipal();
+
+            while (true)
+            {
+                telaPrincipal.ApresentarMenuPrincipal();
+
+                ITelaCrud telaSelecionada = telaPrincipal.ObterTela();
+
+                char opcaoEscolhida = telaSelecionada.ApresentarMenu();
+
+                switch (opcaoEscolhida)
+                {
+                    case '1': telaSelecionada.CadastrarRegistro(); break;
+
+                    case '2': telaSelecionada.EditarRegistro(); break;
+
+                    case '3': telaSelecionada.ExcluirRegistro(); break;
+
+                    case '4': telaSelecionada.VisualizarRegistros(true); break;
+
+                    default: break;
+                }
+            }
         }
     }
 }
---
+++ ControleDeMedicamentos.ConsoleApp/Util/TelaPrincipal.cs
@@ -0,0 +1,38 @@
+﻿using ControleDeMedicamentos.ConsoleApp.Compartilhado;
+
+namespace ControleDeMedicamentos.ConsoleApp.Util;
+
+public class TelaPrincipal
+{
+    private char opcaoPrincipal;
+
+    private ContextoDados contexto;
+
+    public TelaPrincipal()
+    {
+        contexto = new ContextoDados(true);
+    }
+
+    public void ApresentarMenuPrincipal()
+    {
+        Console.Clear();
+
+        Console.WriteLine("------------------------------------------");
+        Console.WriteLine("|        Controle de Medicamentos        |");
+        Console.WriteLine("------------------------------------------");
+
+        Console.WriteLine();
+
+        Console.WriteLine("S - Sair");
+
+        Console.WriteLine();
+
+        Console.Write("Escolha uma das opções: ");
+        opcaoPrincipal = Console.ReadLine()[0];
+    }
+
+    public ITelaCrud ObterTela()
+    {
+        return null;
+    }
+}
