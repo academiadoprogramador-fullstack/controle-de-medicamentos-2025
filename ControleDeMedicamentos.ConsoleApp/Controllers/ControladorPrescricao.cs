1069a19e3f777599115ef8e0ad63267214877ea3
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-05-27 16:01:56 -0300

    Implementado Cadastro de Prescrições

--- ControleDeMedicamentos.ConsoleApp/Controllers/ControladorPrescricao.cs
+++ ControleDeMedicamentos.ConsoleApp/Controllers/ControladorPrescricao.cs
@@ -1,7 +1,11 @@
 ﻿using ControleDeMedicamentos.ConsoleApp.Compartilhado;
+using ControleDeMedicamentos.ConsoleApp.Extensions;
 using ControleDeMedicamentos.ConsoleApp.Model;
+using ControleDeMedicamentos.ConsoleApp.ModuloMedicamento;
+using ControleDeMedicamentos.ConsoleApp.ModuloPaciente;
 using ControleDeMedicamentos.ConsoleApp.ModuloPrescricao;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
 
 namespace ControleDeMedicamentos.ConsoleApp.Controllers;
 
@@ -10,11 +14,95 @@ public class ControladorPrescricao : Controller
 {
     private ContextoDados contexto;
     private IRepositorioPrescricao repositorioPrescricao;
+    private IRepositorioPaciente repositorioPaciente;
+    private IRepositorioMedicamento repositorioMedicamento;
 
     public ControladorPrescricao()
     {
         contexto = new ContextoDados(true);
         repositorioPrescricao = new RepositorioPrescricaoEmArquivo(contexto);
+        repositorioPaciente = new RepositorioPacienteEmArquivo(contexto);
+        repositorioMedicamento = new RepositorioMedicamentoEmArquivo(contexto);
+    }
+
+    [HttpGet("cadastrar")]
+    public IActionResult Cadastrar()
+    {
+        var pacientes = repositorioPaciente.SelecionarRegistros();
+        var medicamentos = repositorioMedicamento.SelecionarRegistros();
+
+        CadastrarPrescricaoViewModel cadastrarVM;
+
+        var prescricaoArmazenada = TempData.Peek("Prescricao");
+
+        if (prescricaoArmazenada != null && prescricaoArmazenada is string jsonString)
+        {
+            cadastrarVM = JsonSerializer.Deserialize<CadastrarPrescricaoViewModel>(jsonString)!;
+
+            cadastrarVM.AdicionarPacientes(pacientes);
+            cadastrarVM.AdicionarMedicamentos(medicamentos);
+
+            return View(cadastrarVM);
+        }
+
+        cadastrarVM = new CadastrarPrescricaoViewModel(pacientes, medicamentos);
+
+        return View(cadastrarVM);
+    }
+
+    [HttpPost("cadastrar")]
+    public IActionResult Cadastrar(CadastrarPrescricaoViewModel cadastrarVM, string acaoSubmit)
+    {
+        var pacientes = repositorioPaciente.SelecionarRegistros();
+        var medicamentos = repositorioMedicamento.SelecionarRegistros();
+
+
+        if (TempData.TryGetValue("Prescricao", out var valor) && valor is string jsonString)
+        {
+            var vmAnterior = JsonSerializer.Deserialize<CadastrarPrescricaoViewModel>(jsonString)!;
+
+            vmAnterior.CrmMedico = cadastrarVM.CrmMedico;
+            vmAnterior.PacienteId = cadastrarVM.PacienteId;
+            vmAnterior.MedicamentoId = cadastrarVM.MedicamentoId;
+            vmAnterior.DosagemMedicamento = cadastrarVM.DosagemMedicamento;
+            vmAnterior.PeriodoMedicamento = cadastrarVM.PeriodoMedicamento;
+
+            cadastrarVM = vmAnterior;
+        }
+
+        if (acaoSubmit == "adicionarMedicamento")
+        {
+            var medicamentoSelecionado = repositorioMedicamento.SelecionarRegistroPorId(cadastrarVM.MedicamentoId);
+
+            var detalhesMedicamentoPrescritoVM = new DetalhesMedicamentoPrescritoViewModel(
+                cadastrarVM.MedicamentoId,
+                medicamentoSelecionado.Nome,
+                cadastrarVM.DosagemMedicamento,
+                cadastrarVM.PeriodoMedicamento,
+                cadastrarVM.QuantidadeMedicamento
+            );
+
+            cadastrarVM.MedicamentosPrescritos.Add(detalhesMedicamentoPrescritoVM);
+
+            TempData["Prescricao"] = JsonSerializer.Serialize(cadastrarVM);
+
+            return RedirectToAction("Cadastrar");
+        }
+        else
+        {
+            var novoRegistro = cadastrarVM.ParaEntidade(pacientes, medicamentos);
+
+            repositorioPrescricao.CadastrarRegistro(novoRegistro);
+
+            NotificacaoViewModel notificacaoVM = new NotificacaoViewModel(
+                "Prescrição Cadastrada!",
+                "A prescrição foi criada com sucesso!"
+            );
+
+            TempData.Remove("Prescricao");
+
+            return View("Notificacao", notificacaoVM);
+        }
     }
 
     [HttpGet("visualizar")]
--- ControleDeMedicamentos.ConsoleApp/Model/PrescricaoViewModels.cs
+++ ControleDeMedicamentos.ConsoleApp/Model/PrescricaoViewModels.cs
@@ -1,7 +1,99 @@
 ﻿using ControleDeMedicamentos.ConsoleApp.Extensions;
+using ControleDeMedicamentos.ConsoleApp.ModuloMedicamento;
+using ControleDeMedicamentos.ConsoleApp.ModuloPaciente;
 using ControleDeMedicamentos.ConsoleApp.ModuloPrescricao;
 
 namespace ControleDeMedicamentos.ConsoleApp.Model;
+
+public class CadastrarPrescricaoViewModel
+{
+    public Guid PacienteId { get; set; }
+    public List<SelecionarPacienteViewModel> PacientesDisponiveis { get; set; }
+
+    public string CrmMedico { get; set; }
+
+    public List<SelecionarMedicamentoViewModel> MedicamentosDisponiveis { get; set; }
+    public List<DetalhesMedicamentoPrescritoViewModel> MedicamentosPrescritos { get; set; }
+
+    public Guid MedicamentoId { get; set; }
+    public string DosagemMedicamento { get; set; }
+    public string PeriodoMedicamento { get; set; }
+    public int QuantidadeMedicamento { get; set; }
+
+    public CadastrarPrescricaoViewModel()
+    {
+        PacientesDisponiveis = new List<SelecionarPacienteViewModel>();
+        MedicamentosDisponiveis = new List<SelecionarMedicamentoViewModel>();
+        MedicamentosPrescritos = new List<DetalhesMedicamentoPrescritoViewModel>();
+    }
+
+    public CadastrarPrescricaoViewModel(List<Paciente> pacientes, List<Medicamento> medicamentos) : this()
+    {
+        AdicionarPacientes(pacientes);
+        AdicionarMedicamentos(medicamentos);
+    }
+
+    public void AdicionarPacientes(List<Paciente> pacientes)
+    {
+        foreach (var p in pacientes)
+        {
+            var selecionarPacienteVM = new SelecionarPacienteViewModel(p.Id, p.Nome);
+
+            PacientesDisponiveis.Add(selecionarPacienteVM);
+        }
+    }
+
+    public void AdicionarMedicamentos(List<Medicamento> medicamentos)
+    {
+        foreach (var m in medicamentos)
+        {
+            var selecionarMedicamentoVM = new SelecionarMedicamentoViewModel(m.Id, m.Nome);
+
+            MedicamentosDisponiveis.Add(selecionarMedicamentoVM);
+        }
+    }
+}
+
+public class SelecionarMedicamentoViewModel
+{
+    public Guid Id { get; set; }
+    public string Nome { get; set; }
+
+    public SelecionarMedicamentoViewModel() { }
+
+    public SelecionarMedicamentoViewModel(Guid id, string nome) : this()
+    {
+        Id = id;
+        Nome = nome;
+    }
+}
+
+public class DetalhesMedicamentoPrescritoViewModel
+{
+    public Guid MedicamentoId { get; set; }
+    public string Medicamento { get; set; }
+    public string Dosagem { get; set; }
+    public string Periodo { get; set; }
+    public int Quantidade { get; set; }
+
+    public DetalhesMedicamentoPrescritoViewModel() { }
+
+    public DetalhesMedicamentoPrescritoViewModel(
+        Guid medicamentoId,
+        string nomeMedicamento,
+        string dosagem,
+        string periodo,
+        int quantidade
+    ) : this()
+    {
+        MedicamentoId = medicamentoId;
+        Medicamento = nomeMedicamento;
+        Dosagem = dosagem;
+        Periodo = periodo;
+        Quantidade = quantidade;
+    }
+}
+
 
 public class VisualizarPrescricoesViewModel
 {
